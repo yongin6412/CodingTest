e228f8632e72a067997c3e972add5d9a5727de6e
yongin <rladls6412@naver.com>
2024-10-30 10:11:08 +0900

    특정한 문자를 대문자로 바꾸기

---
+++ lv_0/특정한문자를대문자로바꾸기.js
@@ -0,0 +1,18 @@
+/**
+ * 문제
+ * 영소문자로 이루어진 문자열 my_string과 영소문자 1글자로 이루어진 문자열 alp가 매개변수로 주어질 때, my_string에서 alp에 해당하는 모든 글자를 대문자로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
+ */
+
+// const solution = (my_string, alp) => {
+//   return [...my_string]
+//     .map((str) => (str === alp ? str.toUpperCase() : str))
+//     .join("");
+// };
+
+// console.log(solution("programmers", "m"));
+
+const solution = (my_string, alp) => {
+  return my_string.replaceAll(alp, alp.toUpperCase());
+};
+
+console.log(solution("programmers", "p"));
