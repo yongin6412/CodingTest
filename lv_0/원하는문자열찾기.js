951bb32f9a1ee35f0bda328d47360a728fef2dbf
yongin <rladls6412@naver.com>
2024-10-31 09:43:52 +0900

    원하는 문자열 찾기

---
+++ lv_0/원하는문자열찾기.js
@@ -0,0 +1,12 @@
+/**
+ * 문제
+ * 알파벳으로 이루어진 문자열 myString과 pat이 주어집니다. myString의 연속된 부분 문자열 중 pat이 존재하면 1을 그렇지 않으면 0을 return 하는 solution 함수를 완성해 주세요.
+
+단, 알파벳 대문자와 소문자는 구분하지 않습니다.
+ */
+
+const solution = (myString, pat) => {
+  return myString.toLowerCase().includes(pat.toLowerCase()) ? 1 : 0;
+};
+
+console.log(solution("aaAA", "aaaaaaa"));
