20409405ea1248d6b1c926a696354d63179596ab
yongin <rladls6412@naver.com>
2024-11-04 10:44:44 +0900

    다른 사람의 풀이

---
+++ lv_0/l로만들기.js
@@ -0,0 +1,18 @@
+/**
+ * 문제
+ * 알파벳 소문자로 이루어진 문자열 myString이 주어집니다. 알파벳 순서에서 "l"보다 앞서는 모든 문자를 "l"로 바꾼 문자열을 return 하는 solution 함수를 완성해 주세요.
+ */
+
+const solution = (myString) => {
+  let result = "";
+  for (let i = 0; i < myString.length; i++) {
+    if (myString[i] <= "l") {
+      result += "l";
+    } else {
+      result += myString[i];
+    }
+  }
+  return result;
+};
+
+console.log(solution("abcdevwxyz"));
