dd55bf5520b9ae7a3226f7329f3c35b13c4fe9b2
yongin <rladls6412@naver.com>
2024-11-04 10:29:39 +0900

    문자 리스트를 문자열로 변환하기

---
+++ lv_0/문자리스트를문자열로변환하기.js
@@ -0,0 +1,10 @@
+/**
+ * 문제
+ * 문자들이 담겨있는 배열 arr가 주어집니다. arr의 원소들을 순서대로 이어 붙인 문자열을 return 하는 solution함수를 작성해 주세요.
+ */
+
+const solution = (arr) => {
+  return arr.join("");
+};
+
+console.log(solution(["a", "b", "c"]));
