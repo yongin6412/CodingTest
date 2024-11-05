acef440ad8c3de9353676edad219449b4e16b6b2
yongin <rladls6412@naver.com>
2024-11-05 09:58:18 +0900

    문자 반복 출력하기

---
+++ lv_0/문자반복출력하기.js
@@ -0,0 +1,11 @@
+/**
+ * 문제
+ * 문자열 my_string과 정수 n이 매개변수로 주어질 때, my_string에 들어있는 각 문자를 n만큼 반복한 문자열을 return 하도록 solution 함수를 완성해보세요.
+ */
+
+const solution = (my_string, n) => {
+  let result = [...my_string].map((str) => str.repeat(n)).join("");
+  return result;
+};
+
+console.log(solution("hello", 3));
