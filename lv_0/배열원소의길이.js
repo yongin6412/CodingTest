93c6969b7315959f3f76c8d02b6cc6d8c92fc695
yongin <rladls6412@naver.com>
2024-11-05 09:59:50 +0900

    배열 원소의 길이

---
+++ lv_0/배열원소의길이.js
@@ -0,0 +1,8 @@
+/**
+ * 문제
+ * 문자열 배열 strlist가 매개변수로 주어집니다. strlist 각 원소의 길이를 담은 배열을 return하도록 solution 함수를 완성해주세요.
+ */
+
+const solution = (strlist) => {
+  return strlist.map((str) => str.length);
+};
