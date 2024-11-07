f1f8011c3d80f3899a0b852a4d924e835e168767
yongin <rladls6412@naver.com>
2024-11-07 10:19:47 +0900

    부분 문자열 이어 붙여 문자열 만들기

---
+++ lv_0/부분문자열이어붙여문자열만들기.js
@@ -0,0 +1,24 @@
+/**
+ * 문제
+ * 길이가 같은 문자열 배열 my_strings와 이차원 정수 배열 parts가 매개변수로 주어집니다. parts[i]는 [s, e] 형태로, my_string[i]의 인덱스 s부터 인덱스 e까지의 부분 문자열을 의미합니다. 각 my_strings의 원소의 parts에 해당하는 부분 문자열을 순서대로 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
+ */
+
+const solution = (my_strings, parts) => {
+  let result = [];
+  for (let i = 0; i < parts.length; i++) {
+    result.push(my_strings[i].slice(parts[i][0], parts[i][1] + 1));
+  }
+  return result.join("");
+};
+
+console.log(
+  solution(
+    ["progressive", "hamburger", "hammer", "ahocorasick"],
+    [
+      [0, 4],
+      [1, 2],
+      [3, 5],
+      [7, 7],
+    ]
+  )
+);
